6142881d959da6990fffdd028adcb8a9aa832697
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-26 20:08:18 +0900

    feat: 페이지네이션

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 const API_NUM = 1717;
-export async function getFoods({ order = "createdAt" }) {
+export async function getFoods({ order = "", cursor = "", limit = 10 }) {
+  const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
   const response = await fetch(
-    `https://learn.codeit.kr/${API_NUM}/foods?order=${order}`
+    `https://learn.codeit.kr/${API_NUM}/foods?${query}`
   );
   const body = await response.json();
   return body;
--- src/components/App.js
+++ src/components/App.js
@@ -6,23 +6,28 @@ import FoodList from "./FoodList";
 function App() {
   const [order, setOrder] = useState("createdAt");
   const [items, setItems] = useState([]);
+  const [cursor, setCursor] = useState(null);
 
-  const handleNewestClick = () => {
-    setOrder("createdAt");
-  };
-
-  const handleCalorieClick = () => {
-    setOrder("calorie");
-  };
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
 
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoad = async (query) => {
-    const { foods } = await getFoods(query);
-    setItems(foods);
-    // setItems((prevItem) => [...prevItem, ...foods]);
+
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) setItems(foods);
+    else setItems((prevItem) => [...prevItem, ...foods]);
+    setCursor(nextCursor);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, cursor });
   };
 
   useEffect(() => {
@@ -34,6 +39,7 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={items} onDelete={handleDelete} />
+      {cursor && <button onClick={handleLoadMore}>더 보기</button>}
     </div>
   );
 }
